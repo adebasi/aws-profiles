2d81850ee7c198e78ca03a0e32725c153d4eb771
Mitja Adebahr <adebasi@users.noreply.github.com>
2021-02-23 22:47:58 +0100

    Avoid exiting current terminal session

--- awsp.sh
+++ awsp.sh
@@ -2,8 +2,7 @@
 
 profile=$(cat ~/.aws/credentials | grep -o '\[[^]]*\]' | awk -F"[][]" '{print $2}' | sort -r | uniq | fzf)
 if [[ -z $profile ]]; then
-    exit 0
-else
-    echo "Set aws profile: $profile"
-    export AWS_PROFILE="$profile"
+    return
 fi
+echo "Set aws profile: $profile"
+export AWS_PROFILE="$profile"
